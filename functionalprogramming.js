200e0ff7320eebfbc751eef9dc1ce8cf2962596f
Tobi Balogun <tobi.balo@gmail.com>
2022-08-17 16:02:18 +0100

    Tasks re-done

--- functionalprogramming.js
+++ functionalprogramming.js
@@ -1,33 +1,37 @@
 // Task 1: Build a function-based console log message generator
 function consoleStyler(color, background, fontSize, txt) {
-    var message = "%c" + txt;
-    var style = `color: ${color};`
-    style += `background: ${background};`
-    style += `font-size: ${fontSize}`;
-    console.log(message, style)
+  const message = "%c" + txt;
+  let style = `color: ${color};`;
+  style += `background: ${background};`;
+  style += `font-size: ${fontSize};`;
+
+  console.log(message, style);
 }
 
 // Task 2: Build another console log message generator
 function celebrateStyler(reason) {
-    var fontStyle = "color: tomato; font-Size: 50px"
-    if(`reason == "birthday"`){
-        console.log(`%cHappy birthday`, fontStyle)
-    }else if(`reason == "champions"`){
-        console.log(`%cCongrats on the title!`, fontStyle)
-    }else{
-        console.log(message, style)
-    }
+  const fontStyle = "color: tomato; font-size: 50px";
+  if (reason == "birthday") {
+    console.log(`%cHappy birthday`, fontStyle);
+  } else if (reason == "champions") {
+    console.log(`%cCongrats on the title!`, fontStyle);
+  } else {
+    console.log(message, style);
+  }
 }
 
 // Task 3: Run both the consoleStyler and the celebrateStyler functions
-console.log(consoleStyler(`'#1d5c63'`,`'#ede6db'`,`'40px'`, `'Congrats!'` ))
-console.log(celebrateStyler(`'birthday'`))
+consoleStyler("#1dc63", "#ede6db", "40px", "Congrats!");
+celebrateStyler("birthday");
 
 // Task 4: Insert a congratulatory and custom message
 function styleAndCelebrate(color, background, fontSize, txt, reason) {
-    consoleStyler(color, background, fontSize, txt);
-    celebrateStyler(reason);
-    return (consoleStyler, celebrateStyler)
+  consoleStyler(color, background, fontSize, txt);
+  celebrateStyler(reason);
 }
+
 // Call styleAndCelebrate
-console.log(styleAndCelebrate(`'ef7c8e'`,`'fae8e0'`,`'30px'`,`'You made it!'`, `'champions'`));
+styleAndCelebrate("#ef7c8e", "#fa38e0", '30px', 'You made it!', 'champions');
+
+// use this if the function in line 34 doesn't work
+// styleAndCelebrate("ef7c8e", "fa38e0", '30px', 'You made it!', 'champions');
